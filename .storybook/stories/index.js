7db1d60b76e0220182fa6b15211305d0e7db67e1
aldeez988 <aldeez911088@gmail.com>
2019-08-20 21:46:35 +0100

    adding button story

---
+++ .storybook/stories/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf, setAddon } from "@storybook/react";
+import CustomButton from "../../src/index";
+
+storiesOf("Custom Button", module).addWithJSX("simple", () => <CustomButton />);
